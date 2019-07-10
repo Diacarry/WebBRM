ddf2fcfca9b44f59de3900bcc89588f0f271ee4f
Andres Carranza <diacarry@gmail.com>
2019-07-10 17:27:53 -0500

    Adición de tres vistas para el aplicativo

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,21 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
-|
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/Admin', function () {
+    return view('admin');
+});
+Route::get('/Proveedor', function () {
+    return view('proveedor');
+});
+Route::get('/Cliente', function () {
+    return view('cliente');
 });
